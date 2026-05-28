e75c9bf08afe62093427dc6a08d29185cd6760d2
Zahegh Murillo <zaheghmurillo2007@gmail.com>
2026-05-28 07:27:16 -0500

    cambios en titulos y estilos

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ $segundoHastaCambio = max(1, 30 - ($tiempoEnCiclo % 30));
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Semaforo</title>
+    <title>Semaforo auto</title>
     <meta http-equiv="refresh" content="<?= $segundoHastaCambio ?>">
 
     <style>
